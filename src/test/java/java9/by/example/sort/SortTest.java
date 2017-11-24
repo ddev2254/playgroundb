b1022ac46e62c879d3f8590346a0ff35e3fd1a0c
ddev2254 <ddev2254@gmail.com>
2017-11-24 21:04:03 +0100

    Add more tests for sort method

--- src/test/java/java9/by/example/sort/SortTest.java
+++ src/test/java/java9/by/example/sort/SortTest.java
@@ -16,4 +16,34 @@ public class SortTest {
         String[] expected = {"aa", "bb", "cc"};
         assertThat(someStrings).isEqualTo(expected);
     }
+
+    @Test
+    public void shouldSortStringsWhenSomeOfThemAreTheSame() {
+        String[] someStrings = {"ff", "ff", "ee"};
+
+        sort(someStrings);
+
+        String[] expected = {"ee", "ff", "ff"};
+        assertThat(someStrings).isEqualTo(expected);
+    }
+
+    @Test
+    public void shouldSortEmptyList() {
+        String[] someStrings = {};
+
+        sort(someStrings);
+
+        String[] expected = {};
+        assertThat(someStrings).isEqualTo(expected);
+    }
+
+    @Test
+    public void shouldSortAlreadySortedList() {
+        String[] someStrings = {"gg", "hh", "ii"};
+
+        sort(someStrings);
+
+        String[] expected = {"gg", "hh", "ii"};
+        assertThat(someStrings).isEqualTo(expected);
+    }
 }
